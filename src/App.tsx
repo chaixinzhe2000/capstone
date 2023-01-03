2efed5f7d4b895c978319eeea1d14179f6c99515
Xinzhe Chai <xinzhe_chai@brown.edu>
2023-01-03 02:42:48 -0500

    refactor some basic stuff

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,19 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/react'
 import { Button, CssVarsProvider } from '@mui/joy';
+import React from 'react';
+import { Renter } from './components';
 import * as styles from './styles'
 
 const App = () => {
 	return (
-		<CssVarsProvider>
+		<React.Fragment>
 			<Button variant="solid">Hello World</Button>
 			<div css={styles.headerWrapper}>
 				hello hello this is proof css works
 			</div>
-		</CssVarsProvider>
+			<Renter />
+		</React.Fragment>
 	);
 }
 
--- src/components/Renter/Renter.tsx
+++ src/components/Renter/Renter.tsx
@@ -1,7 +1,6 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
 import { jsx } from '@emotion/react'
-import { Button, CssVarsProvider } from '@mui/joy';
 import * as styles from './styles'
 
 export const Renter = () => {
