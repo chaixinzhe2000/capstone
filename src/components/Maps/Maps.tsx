4b47e27cb51452ecbc5663481d9a3dd7193272a8
Khosrow Arian <khosrow_arian@brown.edu>
2023-01-23 00:34:06 -0500

    done deployment

--- src/components/Maps/Maps.tsx
+++ src/components/Maps/Maps.tsx
@@ -3,10 +3,13 @@
 import { jsx } from '@emotion/react'
 import { style } from '@mui/system';
 import * as styles from './styles'
-import mapboxgl from 'mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 import { useRef, useState, useEffect } from 'react';
 
-mapboxgl.accessToken = 'pk.eyJ1IjoiY2hhaXhpbnpoZSIsImEiOiJjbGNpcGpvNTY1aG5qM3JxbWp3c2w2Ym8wIn0.ZGDOGq-r7uveFAxbbAETtw';
+import mapboxgl from 'mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+const newMapBoxGL: any = mapboxgl
+// eslint-disable-next-line import/no-webpack-loader-syntax
+newMapBoxGL.workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default;
+newMapBoxGL.accessToken = 'pk.eyJ1IjoiY2hhaXhpbnpoZSIsImEiOiJjbGNpcGpvNTY1aG5qM3JxbWp3c2w2Ym8wIn0.ZGDOGq-r7uveFAxbbAETtw';
 
 export const Maps = () => {
 
@@ -19,7 +22,7 @@ export const Maps = () => {
 
 	useEffect(() => {
 		if (map.current) return; // initialize map only once
-		map.current = new mapboxgl.Map({
+		map.current = new newMapBoxGL.Map({
 			container: mapContainer.current,
 			style: 'mapbox://styles/chaixinzhe/clciqcsxy002814qrntw70lmm',
 			center: [lng, lat],
